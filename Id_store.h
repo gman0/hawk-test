4d9787cc667f498d43e3f0a2134c0d086eb6db7d
gman <gman@codefreax.org>
2015-08-03 15:05:03 +0200

    Added the GPL license preamble

--- Id_store.h
+++ Id_store.h
@@ -1,3 +1,22 @@
+/*
+	Copyright (C) 2013-2015 Róbert "gman" Vašek <gman@codefreax.org>
+
+	This file is part of hawk-test.
+
+	hawk-test is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	(at your option) any later version.
+
+	hawk-test is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with hawk-test.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef _id_store_h_
 #define _id_store_h_
 
